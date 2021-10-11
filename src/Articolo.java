af780b8dfa0ebc63d02aa03f8b6ae88ede2a3ce3
Davide Corsi <davide.corsi@par-tec.it>
2021-10-11 15:36:50 +0200

    risoluzione

--- src/Articolo.java
+++ src/Articolo.java
@@ -2,4 +2,8 @@
  * Definisci un'iterfaccia articolo con i seguenti metodi: getNome, getPrezzo, toString.
  */
 public interface Articolo {
+	
+	public String getNome();
+	public double getPrezzo();
+	public String toString();
 }
--- src/Catalogo.java
+++ src/Catalogo.java
@@ -6,6 +6,38 @@ import java.util.List;
  * La classe catalogo rappresenta un catalogo di articoli commerciali, quindi memorizza gli articoli 
  * in una lista. Definire poi i vari metodi.
  */
-public class Catalogo {
+public class Catalogo<E extends Articolo> {
 	
+	private List<E> catalogo;
+	
+	public Catalogo() {
+		this.catalogo = new ArrayList<E>();
+	}
+	
+	void aggiungiArticolo(E art) {
+		this.catalogo.add(art);
+	}
+	
+	List<E> getArticoliSottoPrezzo(double prezzo) {
+		List<E> articoli = new ArrayList<E>();
+		Iterator<E> it = this.catalogo.iterator();
+		while(it.hasNext()) {
+			E el = it.next();
+			if(el.getPrezzo() < prezzo) {
+				articoli.add(el);
+			}
+		}
+		return articoli;
+	}
+	
+	public String toString() {
+		String s="Catalogo articoli";
+		s += "---------------------\n";
+		Iterator<E> it = this.catalogo.iterator();
+		while(it.hasNext()) {
+			E el=it.next();
+			s+=el.toString()+"\n";
+		}
+		return s;
+	}
 }
