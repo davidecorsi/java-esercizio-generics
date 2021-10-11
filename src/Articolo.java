a5fc279cba8fa81e9c0a66ce1c22e7a98868799c
Davide Corsi <davide.corsi@par-tec.it>
2021-10-11 15:36:02 +0200

    first commit

---
+++ src/Articolo.java
@@ -0,0 +1,5 @@
+/*
+ * Definisci un'iterfaccia articolo con i seguenti metodi: getNome, getPrezzo, toString.
+ */
+public interface Articolo {
+}
---
+++ src/Catalogo.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/*
+ * La classe catalogo rappresenta un catalogo di articoli commerciali, quindi memorizza gli articoli 
+ * in una lista. Definire poi i vari metodi.
+ */
+public class Catalogo {
+	
+}
