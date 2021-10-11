3e7cb980bda2dcc7ab4eed9bea2093853ecafa7c
Davide Corsi <davide.corsi@par-tec.it>
2021-10-11 15:42:10 +0200

    risoluzione

---
+++ src/Mobile.java
@@ -0,0 +1,27 @@
+/*
+ * Definire la classe Mobile che implementa l'interfaccia Articolo. Definire poi tutti i metodi.
+ */
+public class Mobile {
+	
+	public Mobile(String nome, double prezzo, double lunghezza,
+			double altezza, double profondità) {
+	}
+	
+	public double getLunghezza() {
+	}
+	
+	public double getAltezza() {
+	}
+	
+	public double getProfondità() {
+	}
+	
+	public String getNome() {
+	}
+	
+	public double getPrezzo() {
+	}
+	
+	public String toString(){
+	}
+}
---
+++ src/ProvaMobile.java
@@ -0,0 +1,9 @@
+/*
+ *La classe ProvaMobile deve essere dotata di un metodo scegliMobile che riceve in ingresso un catalogo di
+ * oggetti di tipo Mobile (o suoi sotto-tipi), un prezzo e tre dimensioni (larghezza, altezza, profondità)
+ * e restituisce un lista di mobili il cui prezzo è inferiore al prezzo dato e le cui dimensioni sono 
+ * inferiori alledimensioni date.
+ */
+public class ProvaMobile {
+
+}
