abee8e44975f82d99c104421326f46c25a73886a
Davide Corsi <davide.corsi@par-tec.it>
2021-10-11 15:50:51 +0200

    risoluzione2

--- src/Mobile.java
+++ src/Mobile.java
@@ -1,27 +1,44 @@
 /*
  * Definire la classe Mobile che implementa l'interfaccia Articolo. 
  */
-public class Mobile {
-	
-	public Mobile(String nome, double prezzo, double lunghezza,
-			double altezza, double profondita) {
+public class Mobile implements Articolo {
+
+	String nome;
+	double prezzo;
+	double lunghezza;
+	double altezza;
+	double profondita;
+
+	public Mobile(String nome, double prezzo, double lunghezza, double altezza, double profondita) {
+		this.nome = nome;
+		this.prezzo = prezzo;
+		this.lunghezza = lunghezza;
+		this.altezza = altezza;
+		this.profondita = profondita;
 	}
-	
+
 	public double getLunghezza() {
+		return lunghezza;
 	}
 	
 	public double getAltezza() {
+		return altezza;
 	}
 	
 	public double getProfondita() {
+		return profondita;
 	}
 	
 	public String getNome() {
+		return this.nome;
 	}
 	
 	public double getPrezzo() {
+		return this.prezzo;
 	}
 	
 	public String toString(){
+		return this.nome + " Prezzo: " + prezzo + " Misure: " + lunghezza + " X " + altezza + " X " 
+				+ profondita;
 	}
 }
--- src/ProvaMobile.java
+++ src/ProvaMobile.java
@@ -1,9 +1,26 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /*
  *La classe ProvaMobile deve essere dotata di un metodo scegliMobile che riceve in ingresso un catalogo di
  * oggetti di tipo Mobile (o suoi sotto-tipi), un prezzo e tre dimensioni (larghezza, altezza, profondità)
  * e restituisce un lista di mobili il cui prezzo è inferiore al prezzo dato e le cui dimensioni sono 
- * inferiori alledimensioni date.
+ * inferiori alle dimensioni date.
  */
 public class ProvaMobile {
-
+	public static <T extends Mobile> List<T> scegliMobile(Catalogo<T> catalogo, double prezzo, 
+			double lunghezza, double altezza, double profondità) {
+		List<T> selezionati = new ArrayList<T>();
+		List<T> selezionatiPerPrezzo = catalogo.getArticoliSottoPrezzo(prezzo);
+		Iterator<T> it = selezionatiPerPrezzo.iterator();
+		while(it.hasNext()) {
+			T el=it.next();
+			if(el.getLunghezza() < lunghezza && el.getAltezza() < altezza 
+					&& el.getProfondita() < profondità) {
+				selezionati.add(el);
+			}
+		}
+		return selezionati;
+	}
 }
