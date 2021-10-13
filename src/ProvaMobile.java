d6ffa1cb51e78eedb2749b36e6c3c9f28e0b24de
Davide Corsi <davide.corsi@par-tec.it>
2021-10-13 22:41:09 +0200

    risoluzione

--- src/ProvaMobile.java
+++ src/ProvaMobile.java
@@ -1,9 +1,16 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 /*
  *La classe ProvaMobile deve essere dotata di un metodo scegliMobile che riceve in ingresso un catalogo di
  * oggetti di tipo Mobile (o suoi sotto-tipi), un prezzo e tre dimensioni (larghezza, altezza, profondità)
  * e restituisce un lista di mobili il cui prezzo è inferiore al prezzo dato e le cui dimensioni sono 
- * inferiori alledimensioni date.
+ * inferiori alle dimensioni date.
  */
 public class ProvaMobile {
+	public static <T extends Mobile> List<T> scegliMobile(Catalogo<T> catalogo, double prezzo, 
+			double lunghezza, double altezza, double profondita) {
+	}
 
 }
