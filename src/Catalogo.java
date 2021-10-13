50770da86700303372fcb935ec1d56e94eacdec3
Davide Corsi <davide.corsi@par-tec.it>
2021-10-13 22:15:36 +0200

    first commit

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -6,6 +6,19 @@ import java.util.List;
  * La classe catalogo rappresenta un catalogo di articoli commerciali, quindi memorizza gli articoli 
  * in una lista. Definire poi i vari metodi.
  */
-public class Catalogo {
+public class Catalogo<E extends Articolo> {
 	
+	private List<E> catalogo;
+	
+	public Catalogo() {
+	}
+	
+	void aggiungiArticolo(E art) {
+	}
+	
+	List<E> getArticoliSottoPrezzo(double prezzo) {
+	}
+	
+	public String toString() {
+	}
 }
